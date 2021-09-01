85c643b4fa5000854a5d1fd1745b98f535378019
Matthew Kennedy <matthewkennedy@outlook.com>
2021-09-01 15:47:12 -0700

    poor man's one touch

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -13,7 +13,32 @@ int main()
     InitCan();
     InitSwitchSense();
 
-    while (1);
+    palSetPadMode(GPIOD, 7, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOG, 10, PAL_MODE_OUTPUT_PUSHPULL);
+
+    while (1)
+    {
+        auto x = SenseSwitch();
+
+        switch (x)
+        {
+            case SwitchState::Open:
+                palWritePad(GPIOD,  7, 1);
+                palWritePad(GPIOG, 10, 0);
+                chThdSleepMilliseconds(20000);
+                break;
+            case SwitchState::Close:
+                palWritePad(GPIOD,  7, 0);
+                palWritePad(GPIOG, 10, 1);
+                chThdSleepMilliseconds(20000);
+                break;
+            default:
+                palWritePad(GPIOD,  7, 0);
+                palWritePad(GPIOG, 10, 0);
+        }
+
+        chThdSleepMilliseconds(100);
+    }
 
     return 0;
 }
